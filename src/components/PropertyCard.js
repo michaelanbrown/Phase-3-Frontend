1088be0d6c4d97500f7fd6c072b951071197e99c
Michaela Brown <m_brownbrown@yahoo.com>
2023-02-28 13:06:22 -0800

    add propertycard component

---
+++ src/components/PropertyCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function PropertyCard() {
+    
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PropertyCard;
