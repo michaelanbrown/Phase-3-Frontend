7317bb62baa57c7c037b90095232a5919b66751b
Michaela Brown <m_brownbrown@yahoo.com>
2023-02-28 13:24:56 -0800

    build out proprtycard component

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 
-function PropertyCard() {
+function PropertyCard({ property }) {
     
 
     return (
         <div>
-            
+            <br></br>
+            <img src={property.link} alt={property.address} width="75%" height="75%"/>
+            <p>{property.city}<br></br>{property.state}</p>
+            <br></br>
+            <br></br>
         </div>
     )
 }
