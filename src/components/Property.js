f494ce110b4224c13f300bd063de21ac9e7fdd4c
Michaela Brown <m_brownbrown@yahoo.com>
2023-03-01 09:49:52 -0800

    error code useContext

--- src/components/Property.js
+++ src/components/Property.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
+import { useParams } from 'react-router-dom';
 
 function Property({ property }) {
+    const { id } = useParams();
+    console.log(id)
 
     return (
         <div>
