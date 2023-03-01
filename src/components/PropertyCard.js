49a4970a99170a99d5f64c4427e04cad2dfb09c0
Michaela Brown <m_brownbrown@yahoo.com>
2023-02-28 19:45:12 -0800

    building on purchasecard component mroe information

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -4,14 +4,21 @@ import './App.css';
 
 function PropertyCard({ property }) {
     const propertyURL = `/properties/${property.id}`
+    const purchasePrice = property.purchase_price ? `$${property.purchase_price}` : "Pending Purchase"
 
     return (
         <div className="PropertyClass">
             <br></br>
             <img className = "PropertyCardImg" src={property.link} alt={property.address} width="75%" height="75%"/>
             <p>
-                {property.city}<br></br>{property.state}<br></br>
-                <NavLink exact to={propertyURL} activeStyle={{color: 'blue'}} style={{color: 'black'}}>Owned</NavLink>
+                {property.street_address}
+                <br/>
+                {property.city}
+                <br/>
+                {property.state}
+                <br/>
+                {purchasePrice}<br/>
+                <NavLink exact to={propertyURL} activeStyle={{color: 'blue'}} style={{color: 'black'}}>View Details</NavLink>
             </p>
         </div>
     )
