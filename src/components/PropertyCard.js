26ba7dd38e4aabf62ffa1654b97e10d00e9225b5
Michaela Brown <m_brownbrown@yahoo.com>
2023-02-28 19:54:25 -0800

    wording changes to propertyCard component

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -5,6 +5,7 @@ import './App.css';
 function PropertyCard({ property }) {
     const propertyURL = `/properties/${property.id}`
     const purchasePrice = property.purchase_price ? `$${property.purchase_price}` : "Pending Purchase"
+    const garageSpaces = property.garage_spaces ? `${property.garage_spaces} garage spaces` : "No garage"
 
     return (
         <div className="PropertyClass">
@@ -17,7 +18,14 @@ function PropertyCard({ property }) {
                 <br/>
                 {property.state}
                 <br/>
-                {purchasePrice}<br/>
+                {purchasePrice}
+                <br/>
+                {property.square_feet} sqft
+                <br/>
+                {garageSpaces}
+                <br/>
+                {property.type.property_type}
+                <br/>
                 <NavLink exact to={propertyURL} activeStyle={{color: 'blue'}} style={{color: 'black'}}>View Details</NavLink>
             </p>
         </div>
