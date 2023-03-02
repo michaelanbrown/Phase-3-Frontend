ac2550b0f6c5eb266a0615208eaa4d1b930c472d
Michaela Brown <m_brownbrown@yahoo.com>
2023-03-01 18:32:50 -0800

    css additions to recordcard.js

--- src/components/RecordCard.js
+++ src/components/RecordCard.js
@@ -1,10 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function RecordCard({ record }) {
 
     return(
-        <div>{record.mortgage_payment}</div>
+        <div className="recordBody">
+            <p>
+            Occupied: {record.occupancy ? "Occupied" : "Vacant"}{'  '}
+            Mortgage Payment: {record.mortgage_payment}{'  '}
+            HOA: {record.hoa_payment ? record.hoa_payment : "No HOA"}{'  '}
+            Property Management: {record.property_management_payment ? record.property_management_payment : "No Property Manager"}
+            </p>
+        </div>
     )
 }
 
