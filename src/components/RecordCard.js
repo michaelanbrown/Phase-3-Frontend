bf79c5b589dde94e0560a85cdb39edc9034864f2
Michaela Brown <m_brownbrown@yahoo.com>
2023-03-06 16:48:46 -0800

    added income to records

--- src/components/RecordCard.js
+++ src/components/RecordCard.js
@@ -10,6 +10,7 @@ function RecordCard({ record }) {
             Mortgage Payment: {record.mortgage_payment}<a style={{ marginLeft: '5rem' }}/>
             HOA: {record.hoa_payment ? record.hoa_payment : "No HOA"}<a style={{ marginLeft: '5rem' }}/>
             Property Management: {record.property_management_payment ? record.property_management_payment : "No Property Manager"}<a style={{ marginRight: '2rem' }}/>
+            Income: {record.gross_income ? record.gross_income : "No income"}<a style={{ marginLeft: '5rem' }}/>
             </p>
         </div>
     )
